c9610b006ec1aa05906a028f061fa74578fa0e83
Georgi Zhelezov <howkinss@gmail.com>
2025-02-13 13:59:56 +0200

    Update logging.php

--- config/logging.php
+++ config/logging.php
@@ -4,6 +4,7 @@ return [
     'elasticsearch' => [
         'driver' => 'monolog',
         'level' => env('LOG_LEVEL', 'debug'),
+        'channel' => 'elasticsearch',
         'elastic' => [
             'host' => [env('ELASTICSEARCH_HOST', 'http://')],
             'ssl_verification' => env('ELASTICSEARCH_SSL_VERIFICATION', false),
